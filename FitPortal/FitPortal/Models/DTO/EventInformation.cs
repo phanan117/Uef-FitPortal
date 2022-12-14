f650fb01dc8feeebde61112ec23d82ed155b94d7
Phan Binh An <114154377+phanan117@users.noreply.github.com>
2022-12-15 00:25:11 +0700

    Showing posts on homepage
    Hiển thị bài đăng trên trang chủ

---
+++ FitPortal/FitPortal/Models/DTO/EventInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace FitPortal.Models.DTO
+{
+    public class EventInformation
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Content { get; set; }
+        public string Picture { get; set; }
+        public string Describe { get; set; }
+    }
+}
